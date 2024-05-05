a006ae07a9858ba0d7a5ddd32e6e609dfa92dca3
MateuszUzi <mateuszusarek92@gmail.com>
2024-05-05 18:22:39 +0200

    Turret New Script

--- Tower Defense Game/Assets/Turret.cs
+++ Tower Defense Game/Assets/Turret.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using System.Collections;
 
 public class Turret : MonoBehaviour {
+
     public Transform target;
     public float range = 15f;
 
@@ -12,6 +13,12 @@ public class Turret : MonoBehaviour {
     void update() {
     }
 
- 
+    private void OnDrawGizmosSelected ()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+        
+    }
+
 
 }
