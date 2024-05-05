08c58d690ce83731dc70bc9b51ff5d5f9b885f02
MateuszUzi <mateuszusarek92@gmail.com>
2024-05-05 17:26:08 +0200

    Wave logic added
    - Enemy added
    - Waves added
    - Timer added
    - Turret template

---
+++ Tower Defense Game/Assets/Turret.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+public class Turret : MonoBehaviour {
+    public Transform target;
+    public float range = 15f;
+
+    private void Start() {
+
+    }
+
+    void update() {
+    }
+
+ 
+
+}
